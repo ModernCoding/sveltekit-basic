62482effb56d2b8a21c8db19efdc561a82f3272e
Cormorant Programming <cormorant.programming@gmail.com>
2022-02-25 20:59:52 +0700

    The first commit

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,24 @@
+<nav>
+
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+
+</nav>
+
+
+<style type="text/css">
+  
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 3px 18px;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+  import Nav from '$lib/Nav.svelte'  
+</script>
+
+
+<Nav />
+
+<slot />
